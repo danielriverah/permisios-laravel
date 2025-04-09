4b78fb3efd157d23fd0328ed956b023993826b29
DANIEL RIVERA HERRADA <108365290+danielriverah@users.noreply.github.com>
2025-04-09 12:44:51 -0600

    migraciones

--- src/PermisosServiceProvider.php
+++ src/PermisosServiceProvider.php
@@ -19,7 +19,10 @@ class PermisosServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/../config/permisos.php' => config_path('permisos.php'),
         ], 'config');
-
+        // Publicar migraciones
+        $this->publishes([
+            __DIR__.'/../database/migrations' => database_path('migrations'),
+        ], 'migrations');
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
         $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'permisos');
