507226c3392a4f7ab832ba76ad9697bd64ca27fa
DANIEL RIVERA HERRADA <108365290+danielriverah@users.noreply.github.com>
2025-04-09 14:26:06 -0600

    migraciones down

--- database/migrations/0009_00_0000001_create_roles_tabla.php
+++ database/migrations/0009_00_0000001_create_roles_tabla.php
@@ -18,6 +18,6 @@ return new class extends Migration
 
     public function down()
     {
-        Schema::dropIfExists('permisos_roles');
+        Schema::dropIfExists('roles');
     }
 };
--- database/migrations/0009_00_0000004_create_permisos_roles_table.php
+++ database/migrations/0009_00_0000004_create_permisos_roles_table.php
@@ -22,6 +22,6 @@ return new class extends Migration
 
     public function down()
     {
-        Schema::dropIfExists('usuario_permisos');
+        Schema::dropIfExists('permisos_roles');
     }
 };
