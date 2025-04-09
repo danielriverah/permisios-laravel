0179f46eed0546cb8c7a16eb1a11a0598fdde623
DANIEL RIVERA HERRADA <108365290+danielriverah@users.noreply.github.com>
2025-04-09 14:22:57 -0600

    migrations

--- database/migrations/0009_00_0000001_create_roles_tabla.php
+++ database/migrations/0009_00_0000001_create_roles_tabla.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRoleTable extends Migration
+return new class extends Migration
 {
     public function up()
     {
@@ -20,4 +20,4 @@ class CreateRoleTable extends Migration
     {
         Schema::dropIfExists('permisos_roles');
     }
-}
+};
